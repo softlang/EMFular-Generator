38f52dc393a3bf4e43ca2c5cb829a081b66dac55
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-09 11:47:39 +0100

    Templates for both services, with assumptions on my folder structure

---
+++ src/templates/services/model.service.ts.template.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ModelService } from 'ngx-emfular-integration';
+import { IoService } from 'ngx-emfular-helper';
+
+import { {{modelName}} } from '../core/{{modelFileName}}';
+import { {{modelName}}HistoryService } from './{{modelFileName}}-history.service';
+
+{{allModelImports}}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class {{modelName}}Service extends ModelService<{{modelName}}> {
+
+  // explicitly use modeling classes to avoid tree-shaking them away:
+  {{antiExtinctionProperties}}
+
+  constructor(
+    historyService: {{modelName}}HistoryService,
+    ioService: IoService,
+) {
+    super(historyService, ioService, {{modelName}});
+  }
+}
