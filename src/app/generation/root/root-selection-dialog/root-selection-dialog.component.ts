e6745b2e62e3f0c5a103b01c426f3cb6375968ef
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-14 05:47:38 +0100

    COmponent just deals with many classes

--- src/app/generation/root/root-selection-dialog/root-selection-dialog.component.ts
+++ src/app/generation/root/root-selection-dialog/root-selection-dialog.component.ts
@@ -1,26 +1,23 @@
-import {Component, Inject} from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import {
   MAT_DIALOG_DATA,
-  MatDialogActions,
-  MatDialogContent,
   MatDialogRef,
-  MatDialogTitle
+  MatDialogModule,
 } from '@angular/material/dialog';
-import {EClassJson} from '../../../parsing/ecore-json';
-import {FormsModule} from '@angular/forms';
-import {MatRadioButton, MatRadioGroup} from '@angular/material/types/radio';
-import {MatButton} from '@angular/material/types/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+
+import { EClassJson } from '../../../parsing/ecore-json';
 
 @Component({
   selector: 'root-selection-dialog',
   imports: [
-    MatDialogActions,
-    MatRadioGroup,
-    MatRadioButton,
     FormsModule,
-    MatDialogContent,
-    MatDialogTitle,
-    MatButton,
+    MatRadioModule,
+    MatButtonModule,
+    MatDialogModule,
   ],
   templateUrl: './root-selection-dialog.component.html',
   styleUrl: './root-selection-dialog.component.css',
@@ -32,14 +29,6 @@ export class RootSelectionDialogComponent {
     @Inject(MAT_DIALOG_DATA) public candidates: EClassJson[],
     private dialogRef: MatDialogRef<RootSelectionDialogComponent>
   ) {}
-
-  isMany(): boolean {
-    return this.candidates.length>1
-  }
-
-  isNone(): boolean {
-    return this.candidates.length === 0;
-  }
 
   confirm() {
     this.dialogRef.close(this.selected);
