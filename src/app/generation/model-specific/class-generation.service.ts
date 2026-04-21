5ae9ef1f96d3211a78f08fdaa17cd21db1b1d924
Susanne Göbel <goebel@uni-koblenz.de>
2026-04-21 16:56:52 +0200

    Move attribute sense making into step after raw parsing, not complete yet

--- src/app/generation/model-specific/class-generation.service.ts
+++ src/app/generation/model-specific/class-generation.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { TemplateLoadService } from '../utils/template-load.service';
 import { PlaceholderReplacerService } from '../utils/place-holder-replacer.service';
 import { ZipService } from '../utils/zip.service';
-import {EEnumJson} from '../../parsing-model/classifier';
-import {EAttributeJson} from '../../parsing-model/structural-feature';
 import {Package} from '../../generation-model/package';
 import {EClass, EDataType, EEnum} from '../../generation-model/classifier';
 import {CrossReferenceHandler} from '../../generation-model/cross-reference-handler';
@@ -137,137 +135,14 @@ export class ClassGenerationService {
     console.error('Types: ## '+types.map(t => t.name))
 
     return cls.attributes
-      .map(a => this.buildAttribute(a, enums, usedEnums, types, usedTypes) )
+      .map(a => this.buildAttribute(a))
       .join('\n\n');
   }
 
-  private buildAttribute(attr: Attribute,  enums: EEnum[], usedEnums: Set<string>, eDataTypes: EDataType[], usedTypes: Set<string>): string {
-      const tsType = this.mapEcoreTypeToTs(attr);
+  private buildAttribute(attr: Attribute): string {
       const optional = attr.lowerBound === 0 ? "?" : "";
-      const isList =  attr.upperBound === -1 || attr.upperBound > 1
-      const typeShortcut = eDataTypes.find(t => t.name == tsType)
-      if (typeShortcut) {
-        usedTypes.add(typeShortcut.name)
-      }
-      const enumInfo = this.findEnum(tsType, enums);
-      if (enumInfo) {
-        usedEnums.add(enumInfo.name)
-      }
-      const initializer = enumInfo
-      ? this.buildEnumInitializer(attr, enumInfo, isList)
-      : this.buildPrimitiveInitializer(attr, tsType, isList);
-
-      return `  @attribute()\n  ${attr.name}${optional}: ${tsType}${initializer};`;
-  }
-
-  private mapEcoreTypeToTs(attr: EAttributeJson): string {
-    const t = attr.type.raw;
-
-    if (t.endsWith("#//EString")) return "string";
-    if (t.endsWith("#//EBoolean") || t.endsWith("#//EBooleanObject")) return "boolean";
-
-    if (
-      t.endsWith("#//EInt") || t.endsWith("#//EIntegerObject") ||
-      t.endsWith("#//ELong") || t.endsWith('#//ELongObject') ||
-      t.endsWith("#//EShort") || t.endsWith("#//EShortObject") ||
-      t.endsWith("#//EFloat") || t.endsWith("#//EFloatObject") ||
-      t.endsWith("#//EDouble") || t.endsWith("#//EDoubleObject")
-    ) return "number";
-
-    if (t.endsWith("#//EDate")) return "Date";
-    if (t.endsWith("#//EByteArray")) return "Uint8Array";
-
-    // enum or custom datatype → short name
-    //todo why not use cleaned version all the time
-    const idx = t.lastIndexOf("#//");
-    return idx >= 0 ? t.substring(idx + 3) : t;
-  }
-
-  private findEnum(typeName: string, enums: EEnumJson[]): EEnumJson | undefined {
-    return enums.find(e => e.name === typeName);
-  }
-
-  private parseMultiDefault(multiDefault: string): string[] {
-    return multiDefault
-      .split(",")
-      .map(s => s.trim())  // remove whitespace
-      .filter(s => s.length > 0);
-  }
-
-  // --- primitive types ----
-
-  private buildPrimitiveInitializer(attr: EAttributeJson, tsType: string, isList: boolean): string {
-    if (attr.defaultValueLiteral !== undefined) {
-      return " = " + this.formatPrimitiveDefault(attr.defaultValueLiteral, tsType, isList);
-    }
-    if (attr.lowerBound === 1) {
-      return " = " + this.emfPrimitiveDefault(tsType, isList);
-    }
-    return "";
-  }
-
-  private formatPrimitiveDefault(defaultAttr: string, tsType: string, isList: boolean): string {
-    if (tsType === "string") {
-      if(isList) {
-        this.parseMultiDefault(defaultAttr)
-      } else
-        return JSON.stringify(defaultAttr);
-    }
-    return defaultAttr;
-  }
-
-  private emfPrimitiveDefault(tsType: string, isList: boolean): string {
-    if(isList) {
-      return "[]"
-    }
-    switch (tsType) {
-      case "string": return '""';
-      case "number": return "0";
-      case "boolean": return "false";
-      case "Date": return "null";
-      case "Uint8Array": return "new Uint8Array()";
-      default: return "undefined";
-    }
-  }
-
-  //---- Enum handling -----
-  private buildEnumInitializer(attr: EAttributeJson, e: EEnumJson, isList: boolean): string {
-    if(isList) {
-      return this.buildEnumListInitializer(attr, e)
-    } else {
-      return this.buildEnumSingleInitializer(attr, e)
-    }
-  }
-
-  private buildEnumListInitializer(attr: EAttributeJson, e: EEnumJson): string {
-    if (attr.defaultValueLiteral !== undefined) {
-      const valuesStr = this.parseMultiDefault(attr.defaultValueLiteral)
-        .map(v => this.getEnumValue(v, e))
-        .join(", ")
-      return ` = [${valuesStr}]`;
-    }
-    if (attr.lowerBound === 1) {
-      return ` = []`;
-    }
-    return "";
-  }
-
-  private buildEnumSingleInitializer(attr: EAttributeJson, e: EEnumJson): string {
-    if (attr.defaultValueLiteral !== undefined) {
-      return ` = ${this.getEnumValue(attr.defaultValueLiteral, e)}`;
-    }
-    if (attr.lowerBound === 1) {
-      return ` = ${this.getEnumDefault(e)}`;
-    }
-    return "";
-  }
-
-  getEnumDefault(e: EEnumJson): string {
-    return `${e.name}.${e.literals[0]}`;
-  }
-
-  getEnumValue(value: string, e: EEnumJson): string {
-    return ` = ${e.name}.${value}`; //todo could sanitize/check
+      const initializer = attr.defaultValueLiteral ? ' = '+attr.defaultValueLiteral : ''
+      return `  @attribute()\n  ${attr.name}${optional}: ${attr.type}${initializer};`;
   }
 
   asAbstract(meta:EClass): string {
--- src/app/parsing2generation/attribute-resolver.ts
+++ src/app/parsing2generation/attribute-resolver.ts
@@ -1,6 +1,175 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {EAttributeJson} from '../parsing-model/structural-feature';
+import {Attribute} from '../generation-model/structural-feature';
+import {EDataType} from '../generation-model/classifier';
+import {EEnumJson} from '../parsing-model/classifier';
+import {AttributeTargetTypes, BuiltInTypeReference, ClassifierReference} from '../generation-model/cross-references';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AttributeResolver {}
+export class AttributeResolver {
+  /*
+    1) call normal resolvers to get the type right
+    2) set default values to correct values (also for enums and own data types)
+  */
+  resolve(attr: EAttributeJson, enums: EEnumJson[], usedEnums: Set<string>, eDataTypes: EDataType[], usedTypes: Set<string>): Attribute {
+
+    const ref = this.createAttrRef(attr);
+    const tsType = this.mapEcoreTypeToTs(attr);
+    const isList =  attr.upperBound === -1 || attr.upperBound > 1
+
+    const typeShortcut = eDataTypes.find(t => t.name == tsType)
+    if (typeShortcut) {
+      usedTypes.add(typeShortcut.name)
+    }
+    const enumInfo = this.findEnum(tsType, enums);
+    if (enumInfo) {
+      usedEnums.add(enumInfo.name)
+    }
+    const typeCase = this.assignType()
+
+    const initializer = enumInfo
+      ? this.buildEnumInitializer(attr, enumInfo, isList)
+      : this.buildPrimitiveInitializer(attr, tsType, isList);
+
+    return {
+      name: attr.name, //todo clean
+      lowerBound: attr.lowerBound,
+      upperBound: attr.upperBound,
+      type: {
+        target: typeCase,
+        reference: ref,
+      },
+      defaultValueLiteral: initializer
+    }
+  }
+
+  private createAttrRef(attr: EAttributeJson): ClassifierReference|BuiltInTypeReference {
+    //todo
+    return {
+      isBuiltIn: true,
+      name: 'todo'
+    }
+  }
+
+  private assignType(): AttributeTargetTypes {
+    return AttributeTargetTypes.any //todo
+  }
+
+  private createInitializer() {
+
+  }
+
+  private mapEcoreTypeToTs(attr: EAttributeJson): string {
+    const t = attr.type.raw;
+
+    if (t.endsWith("#//EString")) return "string";
+    if (t.endsWith("#//EBoolean") || t.endsWith("#//EBooleanObject")) return "boolean";
+
+    if (
+      t.endsWith("#//EInt") || t.endsWith("#//EIntegerObject") ||
+      t.endsWith("#//ELong") || t.endsWith('#//ELongObject') ||
+      t.endsWith("#//EShort") || t.endsWith("#//EShortObject") ||
+      t.endsWith("#//EFloat") || t.endsWith("#//EFloatObject") ||
+      t.endsWith("#//EDouble") || t.endsWith("#//EDoubleObject")
+    ) return "number";
+
+    if (t.endsWith("#//EDate")) return "Date";
+    if (t.endsWith("#//EByteArray")) return "Uint8Array";
+
+    // enum or custom datatype → short name
+    //todo why not use cleaned version all the time
+    const idx = t.lastIndexOf("#//");
+    return idx >= 0 ? t.substring(idx + 3) : t;
+  }
+
+  private findEnum(typeName: string, enums: EEnumJson[]): EEnumJson | undefined {
+    return enums.find(e => e.name === typeName);
+  }
+
+  private parseMultiDefault(multiDefault: string): string[] {
+    return multiDefault
+      .split(",")
+      .map(s => s.trim())  // remove whitespace
+      .filter(s => s.length > 0);
+  }
+
+  // --- primitive types ----
+
+  private buildPrimitiveInitializer(attr: EAttributeJson, tsType: string, isList: boolean): string {
+    if (attr.defaultValueLiteral !== undefined) {
+      return " = " + this.formatPrimitiveDefault(attr.defaultValueLiteral, tsType, isList);
+    }
+    if (attr.lowerBound === 1) {
+      return " = " + this.emfPrimitiveDefault(tsType, isList);
+    }
+    return "";
+  }
+
+  private formatPrimitiveDefault(defaultAttr: string, tsType: string, isList: boolean): string {
+    if (tsType === "string") {
+      if(isList) {
+        this.parseMultiDefault(defaultAttr)
+      } else
+        return JSON.stringify(defaultAttr);
+    }
+    return defaultAttr;
+  }
+
+  private emfPrimitiveDefault(tsType: string, isList: boolean): string {
+    if(isList) {
+      return "[]"
+    }
+    switch (tsType) {
+      case "string": return '""';
+      case "number": return "0";
+      case "boolean": return "false";
+      case "Date": return "null";
+      case "Uint8Array": return "new Uint8Array()";
+      default: return "undefined";
+    }
+  }
+
+  //---- Enum handling -----
+  private buildEnumInitializer(attr: EAttributeJson, e: EEnumJson, isList: boolean): string {
+    if(isList) {
+      return this.buildEnumListInitializer(attr, e)
+    } else {
+      return this.buildEnumSingleInitializer(attr, e)
+    }
+  }
+
+  private buildEnumListInitializer(attr: EAttributeJson, e: EEnumJson): string {
+    if (attr.defaultValueLiteral !== undefined) {
+      const valuesStr = this.parseMultiDefault(attr.defaultValueLiteral)
+        .map(v => this.getEnumValue(v, e))
+        .join(", ")
+      return ` = [${valuesStr}]`;
+    }
+    if (attr.lowerBound === 1) {
+      return ` = []`;
+    }
+    return "";
+  }
+
+  private buildEnumSingleInitializer(attr: EAttributeJson, e: EEnumJson): string {
+    if (attr.defaultValueLiteral !== undefined) {
+      return ` = ${this.getEnumValue(attr.defaultValueLiteral, e)}`;
+    }
+    if (attr.lowerBound === 1) {
+      return ` = ${this.getEnumDefault(e)}`;
+    }
+    return "";
+  }
+
+  getEnumDefault(e: EEnumJson): string {
+    return `${e.name}.${e.literals[0]}`;
+  }
+
+  getEnumValue(value: string, e: EEnumJson): string {
+    return ` = ${e.name}.${value}`; //todo could sanitize/check
+  }
+
+
+}
